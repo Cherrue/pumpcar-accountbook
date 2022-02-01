4822897cdb4a11970fb787aca11d68aa9e5f8be0
Cherrue <th885172@gmail.com>
2022-02-01 23:15:51 +0900

    [refactor] YearButtonCalendarWidget 삭제 ui 승격 제거 소스코드 제거

--- yearbuttoncalendarwidget.py
+++
@@ -1,6 +0,0 @@
-from PyQt5.QtWidgets import QCalendarWidget
-
-
-class YearButtonCalendarWidget(QCalendarWidget):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
